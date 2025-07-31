da62b586fc6970b91657ee746adde08a1abe6890
Keiliel-Oliveira <keilieloliveira.2004@gmail.com>
2025-07-31 10:18:59 -0300

    Iniciando o projeto
    Preparações iniciais do ambiente.

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,36 @@
+<?php
+
+use PhpCsFixer\Finder;
+use PhpCsFixer\Config;
+
+$finder = new Finder()->in(__DIR__);
+
+$customRules = [
+    'braces_position' => [
+        'classes_opening_brace' => 'same_line',
+        'functions_opening_braces' => 'same_line'
+    ],
+    'class_attributes_separation' => true,
+    'ordered_class_elements' => true,
+    'ordered_interfaces' => true,
+    'empty_loop_body' => [
+        'style' => 'braces'
+    ],
+    'declare_equal_normalize' => [
+        'space' => 'single'
+    ],
+    'concat_space' => [
+        'spacing' => 'one'
+    ],
+    'spaces_inside_parentheses' => [
+        'space' => 'single'
+    ]
+];
+
+return new Config()
+->setRules([
+    "@Symfony" => true,
+    "@PSR12" => true,
+    "@PhpCsFixer" => true
+])
+->setFinder($finder);
